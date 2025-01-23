8c8349d408e53b3521f0c7f9c88a38f1873001ba
RahatIT786 <rahatit486@gmail.com>
2025-01-23 18:23:32 +0530

    index sdd

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -1,12 +1,6 @@
 <!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
-{{-- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Default Title')</title>
-    <!-- Add your CSS/JS links here -->
-</head> --}}
 
 <head>
     <meta charset="UTF-8">
