c5f5ed49191e06deacd5e7ffcc3cedcd0964f63b
abu <rahatit486@gmail.com>
2025-01-23 12:53:02 +0530

    layouts fixed

---
+++ resources/views/user/layouts/navbar.blade.php
@@ -0,0 +1,109 @@
+<nav>
+    <!-- HEADER MAIN MENU AREA -->
+    <div class="mobile_logo_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mobilemenu_con">
+                        <div class="mobile_menu_logo text-left">
+                            <a href="#" title="Tramo">
+<img src="/public/img/AIHUT_LOGO.png" style="height:6rem;" alt="aihut" />
+</a>
+                        </div>
+                        <div class="mobile_menu_option">
+                            <div class="mobile_menu_o mobile_opicon">
+                                <i class="ti-menu openclass"></i>
+                            </div>
+                            <!--SEARCH FORM-->
+                            <div class="mobile_menu_inner mobile_p">
+                                <div class="mobile_menu_content">
+                                    <div class="mobile_menu_logo text-left">
+                                        <a href="#" title="Tramo">
+<img src="/public/img/AIHUT_LOGO.png" alt="aihut" style="height:6rem;"/>
+</a>
+                                    </div>
+                                    <div class="menu_area mobile-menu ">
+                                        <nav>
+                                            <ul id="menu-main-menu" class="main-menu mobile_menu_item clearfix">
+                                                <li id="menu-item-11595" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11595"><a href="../index.php">Get Started</a></li>
+                                                <li id="menu-item-11572" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11572"><a href="#">agents</a></li>
+                                                <li id="menu-item-11585" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11585"><a href="#">distributors</a></li>
+                                                <li id="menu-item-11600" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11600"><a href="#">suppliers</a></li>
+                                                <li id="menu-item-11599" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11599"><a href="#">testimonials</a></li>
+                                                <li id="menu-item-11604" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-11552 current_page_item menu-item-11604"><a href="../contact.php" aria-current="page">contact us</a></li>
+                                            </ul>
+                                        </nav>
+                                        <div class="mobile_menu_items ">
+                                            <div class="p_menu_social"><a class="social-icon" href="#"><i class="ti-facebook"></i></a><a class="social-icon" href="#"><i class="ti-x fa-brands fa-x-twitters"></i></a><a class="social-icon" href="#"><i class="ti-instagram"></i></a>
+                                                <a
+                                                    class="social-icon" href="#"><i class="ti-pinterest"></i></a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="mobile_menu_o mobile_cicon">
+                                        <i class="ti-close closeclass"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mobile_overlay"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- HEADER MAIN MENU AREA -->
+    <div class="tx_relative_m">
+        <div class="">
+            <div class="mainmenu_width_tx  ">
+                <div class="tramo-main-menu transprent-menu heading_style_4 witr_h_h5">
+                    <div class="trp_nav_area">
+                        <div class="container-fluid">
+                            <div class="row logo-left">
+                                <!-- LOGO -->
+                                <div class="col-xl-2 col-lg-3 col-md-2 col-sm-12">
+
+                                    <div class="logo">
+                                        <a href="#" title="Tramo">
+<img src="/public/img/AIHUT_LOGO.png" alt="aihut" style="height:6rem;" />
+</a>
+                                    </div>
+                                </div>
+                                <!-- MAIN MENU -->
+                                <div class="col-xl-10 col-lg-9 col-md-10 col-sm-12">
+                                    <nav class="tramo_menu">
+                                        <ul id="menu-main-menu-1" class="sub-menu">
+                                            <li class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11595"><a href="../index.php">Get started</a></li>
+                                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11572"><a href="#">agents</a></li>
+                                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11585"><a href="#"> distributors </a></li>
+                                            <li class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11600"><a href="#">suppliers</a></li>
+                                            <li class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-11599"><a href="#">testimonials</a></li>
+                                            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-11552 current_page_item menu-item-11604"><a href="../contact.php" aria-current="page">contact us</a></li>
+                                        </ul>
+                                        <div class="tx_right_otherx d-flex align-items-center">
+                                            <div class="p_menu_social"><a class="social-icon" href="#"><i class="ti-facebook"></i></a><a class="social-icon" href="#"><i class="ti-x fa-brands fa-x-twitters"></i></a><a class="social-icon" href="#"><i class="ti-instagram"></i></a>
+                                                <a
+                                                    class="social-icon" href="#"><i class="ti-pinterest"></i></a>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                </div>
+                                <!-- END MAIN MENU -->
+                            </div>
+                            <!-- END ROW -->
+                        </div>
+                        <!-- END CONTAINER -->
+                    </div>
+                    <!-- END AREA -->
+                </div>
+
+            </div>
+        </div>
+        <!-- absulate div -->
+    </div>
+
+
+    <!-- HEADER MAIN MENU AREA END -->
+
+</nav>
