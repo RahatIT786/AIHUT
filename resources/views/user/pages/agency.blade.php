555d404152331e975e54411e17da72cd9c6baa8d
abu <rahatit486@gmail.com>
2025-01-24 10:45:45 +0530

    agecy nav bar

---
+++ resources/views/user/pages/agency.blade.php
@@ -0,0 +1,257 @@
+@extends('user.layouts.app')
+
+@section('title', 'AIHUT')
+
+@section('content')
+<div class="template-home-wrapper" style="padding-top:100px !important">
+
+	<div class="page-content-home-page">										
+		<div data-elementor-type="wp-page" data-elementor-id="17066" class="elementor elementor-17066">
+        <div class="elementor-element elementor-element-4fc85d4 e-flex e-con-boxed e-con e-parent" data-id="4fc85d4" data-element_type="container">
+					<div class="e-con-inner">
+		<div class="elementor-element elementor-element-00f9a3a e-con-full e-flex e-con e-child" data-id="00f9a3a" data-element_type="container">
+				<div class="elementor-element elementor-element-3ef1bb4 elementor-widget elementor-widget-tts" data-id="3ef1bb4" data-element_type="widget" data-widget_type="tts.default">
+				<div class="elementor-widget-container">
+								<!-- title left -->
+					<div class="tts text-left">
+						<div class="ttin">
+						
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+							
+							<!-- title top -->
+							
+
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+
+							<!-- title middle -->
+															<h2 class="txbdstitle sttwo hlight">Start your <span>Hajj & Umrah</span> Agency</h2>
+														<!-- title bottom -->
+							
+														
+							<!-- image -->
+														<!-- icon -->
+														
+							<!-- bar -->
+														<!-- circle bar -->
+														
+								
+							
+							<!-- content -->
+															<p>This is the perfect time to become a franchise partner with Rahat Group. In an industry that continues to grow rapidly, pilgrimages like Hajj, Umrah, and Ziyarat are seeing unprecedented demand. By partnering with India's National Umrah Brand, you’ll have access to the most competitive packages, cutting-edge technology, and the support of over 1500 experienced travel professionals.</p>		
+								
+						</div>
+							<!-- inner title -->
+													
+					</div>				
+						</div>
+				</div>
+		<div class="elementor-element elementor-element-9431847 e-con-full e-flex e-con e-child" data-id="9431847" data-element_type="container">
+		<div class="elementor-element elementor-element-195e06b e-con-full e-flex e-con e-child" data-id="195e06b" data-element_type="container">
+				<div class="elementor-element elementor-element-b540a2f elementor-widget elementor-widget-tts" data-id="b540a2f" data-element_type="widget" data-widget_type="tts.default">
+				<div class="elementor-widget-container">
+								<!-- title left -->
+                     <div class="txbdsbtn allcostyle">
+                         <a class="btnallt btnall btnallactive " href="/contact.php">REGISTER AS A FRANCHISER</a>
+                     </div>			
+				</div>
+				</div>
+			 </div>
+			</div>
+		</div>
+		<div class="elementor-element elementor-element-b9b727e e-con-full e-flex e-con e-child" data-id="b9b727e" data-element_type="container">
+				<div class="elementor-element elementor-element-899a03b elementor-widget elementor-widget-witrsimage" data-id="899a03b" data-element_type="widget" data-widget_type="witrsimage.default">
+				<div class="elementor-widget-container">
+				
+		<div class="single_image_area">
+			<div class="single_image single_line_option  ">
+				<!-- image -->
+				<img loading="lazy" decoding="async" width="351" height="302" src="https://demo.themexbd.com/sk/tramo/wp-content/uploads/2024/09/layer.png" class="attachment-large size-large wp-image-27468" alt="layer" />		
+											</div>
+		</div>
+			
+			
+		
+			
+				</div>
+				</div>
+				</div>
+					</div>
+		</div>
+		    <div class="elementor-element elementor-element-b1a2b17 e-con-full e-flex e-con e-parent" data-id="b1a2b17" data-element_type="container">
+		    <div class="elementor-element elementor-element-0bd7e23 e-con-full e-flex e-con e-child" data-id="0bd7e23" data-element_type="container" style="background-image: url({{asset('images/makkah.jpg')}});">
+			<div class="elementor-element elementor-element-5295f52 elementor-widget elementor-widget-witr_section_video" data-id="5295f52" data-element_type="widget" data-widget_type="witr_section_video.default">
+			<div class="elementor-widget-container">
+			
+        <div class="video-part videoboxposi">
+			<div class="video-overlay witr_all_color_v">
+				<div class="video-item text-center">
+				<a class="btnallt videowh video-popup video-vemo-icon venobox " data-vbtype="video" data-autoplay="true" href="https://www.youtube.com/embed/k4B9lrd1fFY?si=azNYkTVH1YjxBWUR">
+						<!-- icon -->
+													<i class="icofont icofont-ui-play"></i>
+												
+					</a>
+					<!-- title -->
+									
+				</div> <!-- video item -->
+			</div> <!-- video overlay -->
+        </div> 
+
+		
+				</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-cdb37ee e-con-full e-flex e-con e-child" data-id="cdb37ee" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+				<div class="elementor-element elementor-element-bdf1f45 elementor-widget elementor-widget-tts" data-id="bdf1f45" data-element_type="widget" data-widget_type="tts.default">
+				<div class="elementor-widget-container">
+								<!-- title center -->
+					<div class="tts text-center">
+						<div class="ttin">
+
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+													
+							<!-- title top -->
+							
+
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+							
+							<!-- title middle -->
+															<h2 class="txbdstitle sttwo hlight">our <span> Agent</span> feedback</h2>
+														<!-- title bottom -->
+							
+														
+							<!-- image -->
+														<!-- icon -->
+														
+							<!-- bar -->
+														<!-- circle bar -->
+														
+								
+							
+							<!-- content -->
+															<p>Positive Experience with Support System</p>		
+														
+						</div>
+							<!-- inner title -->
+													
+					</div>
+
+						</div>
+				</div>
+				<div class="elementor-element elementor-element-7024241 elementor-widget elementor-widget-witrtestiw" data-id="7024241" data-element_type="widget" data-widget_type="witrtestiw.default">
+				<div class="elementor-widget-container">
+					<div class="witr_carousel_main row witr_slick_active row" dir="ltr" data-witrshowdata="{&quot;witr_infiv&quot;:true,&quot;witr_stsv&quot;:1,&quot;witr_stsr&quot;:1,&quot;witr_apv&quot;:true,&quot;witr_apsv&quot;:6000,&quot;witr_spv&quot;:1000,&quot;witr_pohv&quot;:true,&quot;witr_cmv&quot;:true,&quot;witr_cpv&quot;:&quot;0px&quot;,&quot;witr_fadev&quot;:false,&quot;witr_arrv&quot;:false,&quot;witr_dotv&quot;:true,&quot;witr_bpdv&quot;:1200,&quot;witr_shv&quot;:1,&quot;witr_bptv&quot;:992,&quot;witr_tshv&quot;:1,&quot;witr_bpmv&quot;:767,&quot;witr_mshv&quot;:1,&quot;witr_bpsmv&quot;:480,&quot;witr_smshv&quot;:1}"style="display:block">
+							<div class="col-md-12  txbdmb30">
+						<div class="tsitem text-center alltesicl">
+							<div class="tscon">
+								<div class="tsimgin tsst4 tsimauto tsmt"><img loading="lazy" decoding="async" width="150" height="150" style="border-radius: 50%;" src="{{asset('images/agent1.png')}}" class="attachment-large size-large wp-image-27377" alt="tes" /></div>							
+								<div class="tsreview tsdflex d-flex align-items-center justify-content-center">
+									<div class="tsrevstar">
+										<i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i>									</div>
+																	</div>							
+																	<p>"Our experience with Rahat Groups has been very positive. The team is incredibly responsive and provides excellent support throughout the process. The communication is clear, and they offer valuable guidance whenever we have questions. The tools and resources they provide make it easy to succeed. We appreciate the professionalism and dedication they show to their partners." </p>		
+																
+							</div>						
+							<div class="tsimg tsst4">
+																<h2 class="tstitle">MD. Mahfuz islam<span class="tesspanf d-block txbdbcolor">Pune</span> </h2>
+																
+							</div>
+						</div>						
+					</div>						<div class="col-md-12  txbdmb30">
+						<div class="tsitem text-center alltesicl">
+							<div class="tscon">
+								<div class="tsimgin tsst4 tsimauto tsmt"><img loading="lazy" decoding="async" width="150" height="150" style="border-radius: 50%;" src="{{asset('images/agent3laddak.png')}}" class="attachment-large size-large wp-image-27377" alt="tes" /></div>							
+								<div class="tsreview tsdflex d-flex align-items-center justify-content-center">
+									<div class="tsrevstar">
+										<i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i><i class="ti-star txbdbcolor"></i>									</div>
+																	</div>							
+																	<p>Working with Rahat Groups has opened up significant opportunities for growth. Their marketing and operational support are commendable, making it easier to build relationships with customers. However, there are occasional delays in response times, which can affect the speed of operations. Despite this, we see a lot of promise and are excited to continue our partnership. Rahat Groups has a strong foundation for success</p>		
+																
+							</div>						
+							<div class="tsimg tsst4">
+																<h2 class="tstitle">MD. Mahfuz islam<span class="tesspanf d-block txbdbcolor"> Laddak </span> </h2>
+																
+							</div>
+						</div>						
+					</div>	</div>		</div>
+				</div>
+				</div>
+				</div>
+
+
+		<div class="elementor-element elementor-element-533ab2b e-con-full e-flex e-con e-parent" data-id="533ab2b" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+		<div class="elementor-element elementor-element-fd5d19f e-con-full e-flex e-con e-child" data-id="fd5d19f" data-element_type="container">
+				<div class="elementor-element elementor-element-9ff5897 elementor-widget elementor-widget-tts" data-id="9ff5897" data-element_type="widget" data-widget_type="tts.default">
+				<div class="elementor-widget-container">
+								<!-- title left -->
+					<div class="tts text-left">
+						<div class="ttin">
+						
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+							
+							<!-- title top -->
+							
+
+							<!-- icon position -->
+								
+							<!-- end icon position -->
+
+							<!-- title middle -->
+														<!-- title bottom -->
+							
+														
+							<!-- image -->
+														<!-- icon -->
+														
+							<!-- bar -->
+														<!-- circle bar -->
+														
+								
+							
+							<!-- content -->
+															<p>Copyright © tramo All Right Reserved </p>		
+								
+						</div>
+							<!-- inner title -->
+													
+					</div>				
+						</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-ccbe23b e-con-full e-flex e-con e-child" data-id="ccbe23b" data-element_type="container">
+				<div class="elementor-element elementor-element-5cdd4fe tramo-star-rating--align-right elementor-widget elementor-widget-witr_footerw" data-id="5cdd4fe" data-element_type="widget" data-widget_type="witr_footerw.default">
+				<div class="elementor-widget-container">
+						<div class="footer_widarea">
+				<div class="witr_fwt">		
+					<!-- title -->
+							
+					<div class="footer-menu">
+						<!-- FOOTER COPYRIGHT MENU -->				
+						 <ul id="menu-footer-menu" class=""><li id="menu-item-19298" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19298"><a href="https://demo.themexbd.com/sk/tramo/about/">About</a></li>
+<li id="menu-item-14450" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14450"><a href="https://demo.themexbd.com/sk/tramo/service/">Service</a></li>
+<li id="menu-item-14449" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14449"><a href="https://demo.themexbd.com/sk/tramo/portfolio-grid/">Portfolio</a></li>
+<li id="menu-item-14448" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14448"><a href="https://demo.themexbd.com/sk/tramo/contact/">Contact</a></li>
+</ul> 
+						
+					</div>					
+				</div>					
+			</div>					
+				</div>
+				</div>
+				</div>
+				</div>
+				</div>
+			
+	</div>
+</div>
+@endsection
+
